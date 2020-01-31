bf467198823f20c8749640f4b1da4958e0f88c75
Hakan Yildiz <hakan@re-coded.com>
2020-01-31 12:27:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function wakeDog(dogName, dogBreed){
 
-console.log(`Wake ${dogName} the ${dogBreedbreed`});
+console.log(`Wake ${dogName} the ${dogBreed}`);
 return (`Wake ${dogName} the ${dogBreed}`);
 }
 

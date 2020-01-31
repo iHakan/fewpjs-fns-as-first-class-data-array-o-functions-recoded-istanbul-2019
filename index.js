c75f31049bbc0919d105b3b4fed82bbfd8749e80
Hakan Yildiz <hakan@re-coded.com>
2020-01-31 12:24:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+function wakeDog(a,b){
+  const a = 'Xerox';
+  const b = 'Border Collie';
+console.log(`Wake ${a} the ${b}`);
+}
+
+function leashDog(dogName, dogBreed){
+  dogName='Boo Radley';
+  dogBreed = 'Pibble';
+console.log(`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed){
+  dogName ='Bunny';
+  dogBreed='Labrador';
+console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+
+function throwFrisbee (dogName, dogBreed){
+  dogName ="Mary J. Blige";
+  dogBreed = 'Pappillon';
+console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName, dogBreed){
+  dogName = 'Santa Paws';
+  dogBreed = 'Husky';
+console.log(`Walk home with ${dogName} the ${dogBreed}`);
+}
+
+function unleashDog(dogName,dogBreed){
+  dogName = 'Lauren';
+  dogBreed = "Golden Retriever";
+console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
